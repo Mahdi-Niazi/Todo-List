6672dccdf7e114a43317f7e53cef1e9b531af23d
Mohammad Mahdi Niazi <m.mahdi.niazi@gmail.com>
2023-07-13 16:59:30 +0430

    Add array objects to index.js files Add iteration to lists.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,49 @@
+import _ from "lodash";
+import "./style.css";
 
-import _ from 'lodash';
-import './style.css';
+const tasks = [
+  {
+    completed: false,
+    description: "Anything",
+    index: 0,
+  },
+  {
+    completed: false,
+    description: "something",
+    index: 1,
+  },
+  {
+    completed: false,
+    description: "everyting",
+    index: 3,
+  },
+];
+console.log(tasks);
 
+const mainList = document.querySelector(".items");
+
+const ul = document.createElement("ul");
+
+mainList.appendChild(ul);
+
+const showTask = (i) => {
+  const li = document.createElement("li");
+  li.classList.add("inside-list");
+  li.innerHTML =
+    "<div class='items-contents'> <input type='checkbox'><p class='txt'>" +
+    tasks[i].description +
+    "</p> </div><i class='fa-solid fa-ellipsis-vertical'></i>";
+
+  ul.appendChild(li);
+};
+tasks.forEach((counter, x) => {
+  showTask(x);
+});
+
+{
+  /* <div class="items-contents">
+                            <input type="checkbox">
+                            <p class="txt">going to parties</p>
+                        </div>
+                        <i class="fa-solid fa-ellipsis-vertical"></i> */
+}

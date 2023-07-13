37d08007fc4fe43cdfd7adfa1c5d169dd8cf08b1
Mohammad Mahdi Niazi <m.mahdi.niazi@gmail.com>
2023-07-13 08:57:18 +0430

    Add html file Add css file add webpack

--- src/index.js
+++ src/index.js
@@ -2,15 +2,3 @@
 import _ from 'lodash';
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
-
-  
-  // lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-
-  return element;
-}
-
-document.body.appendChild(component());

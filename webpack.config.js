560a836a3649a81be0ac6208f4dc3e204d223a51
Mohammad Mahdi Niazi <m.mahdi.niazi@gmail.com>
2023-07-13 17:00:50 +0430

    Add linters

--- webpack.config.js
+++ webpack.config.js
@@ -2,38 +2,38 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    devServer: {
-        static: {
-            directory: path.resolve(__dirname, 'dist')
-        },
-        port: 3000, 
-        open: true, 
-        hot: true,
-        compress: true,
+  mode: 'development',
+  entry: './src/index.js',
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
     },
-    plugins: [
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+  },
+  plugins: [
     new HtmlWebpackPlugin({
-        template: './src/index.html',
+      template: './src/index.html',
     }),
   ],
-    output: {
-        filename: "[name].js",
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
-    },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
 
-    optimization: {
-        runtimeChunk: 'single',
-    },
-    
-    module: {
-        rules: [
-        {
-            test: /\.css$/i,
-            use: ['style-loader', 'css-loader'],
-        },
-        ],
-     },
- };
+  optimization: {
+    runtimeChunk: 'single',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+};

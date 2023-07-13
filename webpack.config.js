356905e933420a0f2037e00cdd094ae3f69c9272
Mohammad Mahdi Niazi <m.mahdi.niazi@gmail.com>
2023-07-13 07:42:25 +0430

    Initialize the project

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'dist')
+        },
+        port: 3000, 
+        open: true, 
+        hot: true,
+        compress: true,
+    },
+    plugins: [
+    new HtmlWebpackPlugin({
+        template: './src/index.html',
+    }),
+  ],
+    output: {
+        filename: "[name].js",
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+
+    optimization: {
+        runtimeChunk: 'single',
+    },
+    
+    module: {
+        rules: [
+        {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+        },
+        ],
+     },
+ };

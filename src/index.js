2a03d3f2cef1547863236b5c3cff60a6fe651122
Mohammad Mahdi Niazi <m.mahdi.niazi@gmail.com>
2023-07-14 19:05:34 +0430

    Add edit function

--- src/index.js
+++ src/index.js
@@ -1,45 +1,72 @@
-import "./style.css";
+import './style.css';
 
-import validateForm from "./functions.js";
-import deleteTask from "./remove.js";
+import validateForm from './functions.js';
+import deleteTask from './remove.js';
 
 let tasks = [];
 
-export const mainList = document.querySelector(".items");
+export const mainList = document.querySelector('.items');
 
 const dataLoading = () => {
-  tasks = JSON.parse(localStorage.getItem("datas")) ?? [];
+  tasks = JSON.parse(localStorage.getItem('datas')) ?? [];
 };
 export default dataLoading;
 
-const ul = document.createElement("ul");
+const ul = document.createElement('ul');
 
 mainList.appendChild(ul);
 
 const showTask = (i) => {
-  const li = document.createElement("li");
-  li.classList.add("inside-list");
-  li.innerHTML = `<div class='items-contents'> <input type='checkbox'><p class='txt'>${tasks[i].description}</p> </div><i class='fa-solid fa-ellipsis-vertical icon'></i>`;
+  const li = document.createElement('li');
+  li.classList.add('inside-list');
 
-  ul.appendChild(li);
-  const icon = document.querySelector(".icon");
-  icon.addEventListener("click", () => {
+  const div = document.createElement('div');
+  div.classList.add('items-contents');
+  li.appendChild(div);
+
+  const chk = document.createElement('input');
+  chk.setAttribute('type', 'checkbox');
+  div.appendChild(chk);
+
+  const paragraph = document.createElement('input');
+  paragraph.setAttribute('type', 'text');
+  paragraph.setAttribute('id', 'taskField');
+  paragraph.classList.add('taskField');
+  paragraph.setAttribute('value', tasks[i].description);
+  paragraph.addEventListener('change', () => {
+    tasks[i].description = paragraph.value;
+    localStorage.setItem('datas', JSON.stringify(tasks));
+    dataLoading();
+  });
+  div.appendChild(paragraph);
+
+  const di = document.createElement('i');
+  di.classList.add('fa-solid', 'fa-ellipsis-vertical', 'icon');
+  di.addEventListener('click', () => {
     deleteTask(i);
     dataLoading();
   });
+  li.appendChild(di);
+
+  ul.appendChild(li);
+  // const icon = document.querySelector(".icon");
+  // icon.addEventListener("click", () => {
+  //   deleteTask(i);
+  //   dataLoading();
+  // });
 };
 
 function component() {
-  const form = document.querySelector("#taskForm");
-  form.addEventListener("submit", () => {
+  const form = document.querySelector('#taskForm');
+  form.addEventListener('submit', () => {
     validateForm();
     dataLoading();
   });
 
-  const inputSubmit = document.createElement("input");
-  inputSubmit.setAttribute("type", "submit");
-  inputSubmit.classList.add("btn-submit");
-  inputSubmit.setAttribute("value", ">");
+  const inputSubmit = document.createElement('input');
+  inputSubmit.setAttribute('type', 'submit');
+  inputSubmit.classList.add('btn-submit');
+  inputSubmit.setAttribute('value', '>');
 
   form.appendChild(inputSubmit);
 
@@ -48,7 +75,7 @@ function component() {
   });
 }
 
-window.addEventListener("DOMContentLoaded", () => {
+window.addEventListener('DOMContentLoaded', () => {
   dataLoading();
   component();
 });
